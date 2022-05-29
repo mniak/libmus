95cec21d8c1fc1a78ae41c9ed03acdb6f8f705b3
Andre Soares <andregsilv@gmail.com>
2022-05-29 17:43:30 -0300

    Fix truncateRange function

--- LibmusCpp/Utils.cpp
+++ LibmusCpp/Utils.cpp
@@ -4,7 +4,7 @@
 #include "Utils.h"
 
 int truncateRange(int value, int min, int max) {
-	value = (value > max) * max + (value < max) * value;
-	value = (value < min) * min + (value > min) * value;
+	value = (value >= max) * max + (value < max) * value;
+	value = (value <= min) * min + (value > min) * value;
 	return value;
 }
