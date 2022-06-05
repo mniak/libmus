09d2524e7e7766aa1ad908acff2872388ce0f014
Andre Soares <andregsilv@gmail.com>
2022-06-05 11:42:45 -0300

    fix doubles

--- src/libmus/constants.h
+++ src/libmus/constants.h
@@ -9,6 +9,6 @@ const vector<u32string> NAMES = {U"C", U"D", U"E", U"F", U"G", U"A", U"B"};
 
 const auto FLAT_SYMBOL = U'\x266D';
 const auto SHARP_SYMBOL = U'\x266F';
-const auto DOUBLE_FLAT_SYMBOL = U'\xD834DD2B';
-const auto DOUBLE_SHARP_SYMBOL = U'\xD834DD2A';
+const auto DOUBLE_FLAT_SYMBOL = U'\x1D12B';
+const auto DOUBLE_SHARP_SYMBOL = U'\x1D12A';
 }
