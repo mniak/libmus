d5af528689ff6c68f4908e138b61774ac7272a7e
Andre Soares <andregsilv@gmail.com>
2023-03-22 19:49:11 -0300

    good main

--- clib/example/main.c
+++ clib/example/main.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     PitchClass pc = NewPitchClass();
 
     char* fullName = PitchClass_PrettyName(pc);
     printf("Name %s <- default value\n", fullName);
+    free(fullName);
 
     PitchClass_SetStep(pc, 2);
+    fullName = PitchClass_PrettyName(pc);
     printf("Name %s <- after change\n", fullName);
     free(fullName);
+
     return 0;
 }
