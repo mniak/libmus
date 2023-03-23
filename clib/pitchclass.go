187148a201f199b91635e9a078ca74a0f6eba6d2
Andre Soares <andregsilv@gmail.com>
2023-03-23 08:09:32 -0300

    format

--- clib/pitchclass.go
+++ clib/pitchclass.go
@@ -4,56 +4,67 @@ package main
 
 import (
 	"runtime/cgo"
+
 	"github.com/mniak/libmus"
 )
+
 // typedef void* PitchClass;
 import "C"
+
 //export NewPitchClass
 func NewPitchClass() C.PitchClass {
 	result := libmus.NewPitchClass()
 	resultC := C.PitchClass(cgo.NewHandle(&result))
 	return resultC
 }
+
 //export PitchClass_GetStep
 func PitchClass_GetStep(handle C.PitchClass) int {
 	this := cgo.Handle(handle).Value().(*libmus.PitchClass)
 	result := this.GetStep()
 	return result
 }
+
 //export PitchClass_SetStep
 func PitchClass_SetStep(handle C.PitchClass, value int) {
 	this := cgo.Handle(handle).Value().(*libmus.PitchClass)
 	this.SetStep(value)
 }
+
 //export PitchClass_GetAlteration
 func PitchClass_GetAlteration(handle C.PitchClass) int {
 	this := cgo.Handle(handle).Value().(*libmus.PitchClass)
 	result := this.GetAlteration()
 	return result
 }
+
 //export PitchClass_SetAlteration
 func PitchClass_SetAlteration(handle C.PitchClass, value int) {
 	this := cgo.Handle(handle).Value().(*libmus.PitchClass)
 	this.SetAlteration(value)
 }
+
 //export RandomPitchClass
 func RandomPitchClass() C.PitchClass {
 	result := libmus.RandomPitchClass()
 	resultC := C.PitchClass(cgo.NewHandle(&result))
 	return resultC
 }
+
 //export ExtendedRandomPitchClass
 func ExtendedRandomPitchClass() C.PitchClass {
 	result := libmus.ExtendedRandomPitchClass()
 	resultC := C.PitchClass(cgo.NewHandle(&result))
 	return resultC
 }
+
 //export ParsePitchClass
 func ParsePitchClass(text *C.char) C.PitchClass {
 	result := libmus.ParsePitchClass(C.GoString(text))
 	resultC := C.PitchClass(cgo.NewHandle(&result))
 	return resultC
 }
+
 //export PitchClass_Name
 func PitchClass_Name(handle C.PitchClass) *C.char {
 	this := cgo.Handle(handle).Value().(*libmus.PitchClass)
@@ -61,6 +72,7 @@ func PitchClass_Name(handle C.PitchClass) *C.char {
 	resultC := C.CString(result)
 	return resultC
 }
+
 //export PitchClass_PrettyName
 func PitchClass_PrettyName(handle C.PitchClass) *C.char {
 	this := cgo.Handle(handle).Value().(*libmus.PitchClass)
@@ -68,6 +80,7 @@ func PitchClass_PrettyName(handle C.PitchClass) *C.char {
 	resultC := C.CString(result)
 	return resultC
 }
+
 //export PitchClass_FullName
 func PitchClass_FullName(handle C.PitchClass) *C.char {
 	this := cgo.Handle(handle).Value().(*libmus.PitchClass)
@@ -75,4 +88,3 @@ func PitchClass_FullName(handle C.PitchClass) *C.char {
 	resultC := C.CString(result)
 	return resultC
 }
-
