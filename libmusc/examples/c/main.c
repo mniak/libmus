2149ca7d3f189116a9124ba292aeac943f3ce17a
Andre Soares <andregsilv@gmail.com>
2024-05-01 10:36:55 -0300

    c++ example

--- libmusc/examples/c/main.c
+++ libmusc/examples/c/main.c
@@ -5,6 +5,5 @@ void main() {
     int a = 3;
     int b = 5;
     int c = add(a, b);
-    // int c = 0;
     printf("Math: %d + %d = %d\n", a, b, c);
 }
--- libmusc/examples/cpp/build.bat
+++ libmusc/examples/cpp/build.bat
@@ -1,3 +1,3 @@
 cl /c main.c
 
-link /OUT:main.exe libmus.lib /MACHINE:X64 /LIBPATH:../../../target/release main.obj
+link /OUT:main.exe libmusc.lib /MACHINE:X64 /LIBPATH:../../target/release main.obj
