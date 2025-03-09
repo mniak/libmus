3ef4c65fca4ffc72f961f7428774f029838bb337
Mniak <me@mniak.dev>
2025-03-09 11:25:14 -0300

    use theme color

--- src/app/verovio-display/verovio-display.component.ts
+++ src/app/verovio-display/verovio-display.component.ts
@@ -1,37 +1,38 @@
-import { Component, ElementRef, Signal, SimpleChanges, WritableSignal, computed, input, signal } from '@angular/core';
+import { Component, ElementRef, Signal, SimpleChanges, ViewEncapsulation, WritableSignal, computed, input, signal } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { outputFromObservable, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { VerovioToolkit } from 'verovio/esm';
 import createVerovioModule from 'verovio/wasm';
-import { Observable, Subject, combineLatestWith, debounceTime, distinctUntilChanged, filter, map, mergeMap } from 'rxjs';
+import { Observable, Subject, combineLatestWith, debounceTime, distinctUntilChanged, filter, map, mergeMap, min } from 'rxjs';
 import { VerovioOptions } from 'verovio';
 
 @Component({
    selector: 'app-verovio-display',
    imports: [],
    templateUrl: './verovio-display.component.html',
-   styleUrl: './verovio-display.component.css'
+   styleUrl: './verovio-display.component.css',
+   encapsulation: ViewEncapsulation.None,
 })
 export class VerovioDisplayComponent {
-
    mei = input("")
    scale = input(1);
    page = input(1);
    rawContent: WritableSignal<SafeHtml> = signal("");
 
-   private verovioToolkit: VerovioToolkit | undefined;
-   // private rawContent : Signal<SafeHtml> = signal("");
-   private size = signal([0, 0]);
-   // private meiLoaded signal("");
-   // });
-
-   constructor(private sanitizer: DomSanitizer, elemRef: ElementRef<HTMLElement>) {
+   constructor(sanitizer: DomSanitizer, elemRef: ElementRef<HTMLElement>) {
       let meiO = toObservable(this.mei);
       let pageO = toObservable(this.page);
-      let sizeO = observeResize(elemRef.nativeElement).pipe(debounceTime(100));
+      let sizeO = observeResize(elemRef.nativeElement).pipe(
+         debounceTime(100),
+         map(size => {
+            return {
+               width: Math.max(size.width, 100),
+               height: Math.max(size.height, 100),
+            }
+         })
+      );
       let loadO: Observable<VerovioToolkit> = new Observable(loadS => {
          this.initVerovio().then(tk => {
-            this.verovioToolkit = tk;
             meiO.subscribe(async mei => {
                await tk.loadData(mei);
                loadS.next(tk);
@@ -41,20 +42,19 @@ export class VerovioDisplayComponent {
       loadO.pipe(
          combineLatestWith(sizeO),
          combineLatestWith(pageO),
-         // distinctUntilChanged(),
+         distinctUntilChanged(),
          map(([[tk, size], page]) => {
             let zoom = 50;
             let factor = 1
-            let opt : VerovioOptions = {
+            let opt: VerovioOptions = {
                scale: zoom,
                pageHeight: (size.height * 100 / zoom) * factor,
                pageWidth: (size.width * 100 / zoom) * factor,
                footer: "none",
             };
-            console.log("options", opt);
             tk.setOptions(opt);
             tk.redoLayout();
-            return tk.renderToSVG(1);
+            return tk.renderToSVG(page);
          }),
          map(svg => sanitizer.bypassSecurityTrustHtml(svg)),
       ).subscribe(svg => {
@@ -68,15 +68,6 @@ export class VerovioDisplayComponent {
       return new VerovioToolkit(module);
    }
 
-}
-
-async function loadMei(tk: VerovioToolkit) {
-   if (tk == undefined) {
-      throw new Error("Verovio toolkit was not initialized");
-   }
-   let resp = await fetch('https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei');
-   let mei = await resp.text()
-   return mei;
 }
 
 async function render(tk: VerovioToolkit, mei: string, page: number) {
